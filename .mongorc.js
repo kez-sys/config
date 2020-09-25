0745d4b98af74d9fd4d7e3992eae9f69f4ae1a76
kjsf <kjsfajutagana@gmail.com>
2020-09-26 01:38:40 +0800

    New promt

--- .mongorc.js
+++ .mongorc.js
@@ -4,12 +4,22 @@
 EDITOR = "vim";
 
 // Prompt
+var username = "";
+var user = db.runCommand({
+  connectionStatus: 1,
+}).authInfo.authenticatedUsers[0];
+
+if (!!user) {
+  username = user.user;
+} else {
+  username = "anonymous";
+}
+
 host = db.serverStatus().host;
+
 upTime = Math.floor(db.serverStatus().uptime / 60);
 documents = db.stats().objects;
 
 prompt = () => {
-  return `${db}@${host} ${upTime}:${documents} $ `;
+  return `${username}@${db} ${host} ${upTime}:${documents} $ `;
 };
-
-//mongo;
