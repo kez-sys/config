06b419e1d0b3af7e206b71b05559f6719394b2bb
kjsf <kjsfajutagana@gmail.com>
2020-09-26 23:08:42 +0800

    Updated prompt()

--- .mongorc.js
+++ .mongorc.js
@@ -4,22 +4,23 @@
 EDITOR = "vim";
 
 // Prompt
-var username = "";
-var user = db.runCommand({
-  connectionStatus: 1,
-}).authInfo.authenticatedUsers[0];
-
-if (!!user) {
-  username = user.user;
-} else {
-  username = "anonymous";
-}
-
-host = db.serverStatus().host;
-
-upTime = Math.floor(db.serverStatus().uptime / 60);
-documents = db.stats().objects;
 
 prompt = () => {
-  return `${username}@${db} ${host} ${upTime}:${documents} $ `;
+  let username = "";
+  let host = db.serverStatus().host;
+  let upTime = Math.floor(db.serverStatus().uptime / 60);
+  let documents = db.stats().objects;
+  let collections = db.stats().collections;
+
+  let user = db.runCommand({
+    connectionStatus: 1,
+  }).authInfo.authenticatedUsers[0];
+
+  if (!!user) {
+    username = user.user;
+  } else {
+    username = "anon";
+  }
+
+  return `${username}@${db} ${host} ${upTime}:${collections}:${documents} $ `;
 };
